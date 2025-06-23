9e08136db3287b9bcfb313076a592ffc89ccca8d
mikeryu98 <mikeryu98@gmail.com>
2025-06-24 00:31:22 +0900

    v1.0.7 stateless switch

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
-let Service, Characteristic, Bridge, Accessory, uuid;
+let Service, Characteristic, Accessory, uuid;
 
 module.exports = (api) => {
     Service        = api.hap.Service;
     Characteristic = api.hap.Characteristic;
-    Bridge         = api.hap.Bridge;
     Accessory      = api.platformAccessory;
     uuid           = api.hap.uuid;
 
@@ -23,7 +22,6 @@ class StRoutinePlatform {
         this.name       = config.name;
         this.routineId  = config.routineId;
         this.token      = config.token;
-        this.buttonName = config.buttonName || 'Run Routine';
         this.api        = api;
 
         if (!this.name || !this.routineId || !this.token) {
@@ -31,57 +29,56 @@ class StRoutinePlatform {
         }
 
         this.api.on('didFinishLaunching', () => {
-            this.publishBridgeWithButton();
+            this.publishBridgeAndSwitch();
         });
     }
 
-    publishBridgeWithButton() {
-        // 1) Create child Bridge
+    publishBridgeAndSwitch() {
+        // Create a Bridge accessory
         const bridgeUUID = uuid.generate(this.name);
-        const childBridge = new Bridge(this.name, bridgeUUID);
-        childBridge
+        const bridgeAcc  = new Accessory(this.name, bridgeUUID);
+        bridgeAcc.category = this.api.hap.Categories.BRIDGE;
+        bridgeAcc
             .getService(Service.AccessoryInformation)
             .setCharacteristic(Characteristic.Manufacturer, 'SmartThings')
-            .setCharacteristic(Characteristic.Model, 'RoutineChildBridge');
+            .setCharacteristic(Characteristic.Model, 'RoutineBridge');
 
-        // 2) Create Button accessory (Stateless Programmable Switch)
-        const buttonUUID = uuid.generate(this.buttonName);
-        const buttonAcc  = new Accessory(this.buttonName, buttonUUID);
-        buttonAcc.category = this.api.hap.Categories.SWITCH;
+        // Create a Switch accessory for the routine
+        const switchUUID = uuid.generate(this.switchName);
+        const switchAcc  = new Accessory(this.switchName, switchUUID);
+        switchAcc.category = this.api.hap.Categories.TELEVISION;
 
-        const buttonSvc = new Service.StatelessProgrammableSwitch(this.buttonName);
-        // Only single press
-        buttonSvc
-            .getCharacteristic(Characteristic.ProgrammableSwitchEvent)
-            .setProps({ maxValue: Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS })
-            .onSet(async () => {
+        const svc = new Service.Switch(this.switchName);
+        svc
+            .getCharacteristic(Characteristic.On)
+            .onSet(async (value) => {
+                if (!value) return;
                 try {
                     await axios.post(
                         `https://api.smartthings.com/v1/scenes/${this.routineId}/execute`,
                         {},
                         { headers: { Authorization: `Bearer ${this.token}` } }
                     );
-                    this.log.info(`Executed routine: ${this.buttonName}`);
+                    this.log.info(`Executed routine: ${this.switchName}`);
                 } catch (err) {
-                    this.log.error(`Error executing ${this.buttonName}`, err);
+                    this.log.error(`Error executing ${this.switchName}`, err);
                     throw new this.api.hap.HapStatusError(
                         this.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
                     );
+                } finally {
+                    svc.updateCharacteristic(Characteristic.On, false);
                 }
-            });
+            })
+            .onGet(() => false);
+        switchAcc.addService(svc);
 
-        buttonAcc.addService(buttonSvc);
-
-        // 3) Attach button to Bridge
-        childBridge.addBridgedAccessory(buttonAcc);
-
-        // 4) Publish only the child Bridge
+        // Publish both bridge and switch as External Accessories
         this.api.publishExternalAccessories(
-            'homebridge-smartthings-routine', // plugin identifier
-            [childBridge]
+            'homebridge-smartthings-routine',
+            [bridgeAcc, switchAcc]
         );
 
-        this.log.info(`Published child Bridge and button: ${this.name}`);
+        this.log.info(`Published bridge and switch: ${this.name}`);
     }
 
     configureAccessory() {}
